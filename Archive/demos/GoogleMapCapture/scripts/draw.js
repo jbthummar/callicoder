c0997f10b5ee93e26271456dd43095fcfc2c96ca
jbthummar <jbthummar@gmail.com>
2015-04-09 01:27:33 +0530

    Add GoogleMapCapture

---
+++ Archive/demos/GoogleMapCapture/scripts/draw.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    var activeType = 'rectangle';
+
+    $(document).ready(function () {
+        var canvasElement = $('#drawing-pad')[0],
+            ctx = canvasElement.getContext('2d');
+
+        $('.tool-item').on('click', function () {
+            var this$ = $(this),
+                data = this$.data(),
+                type = data.type;
+
+            if (type !== activeType) {
+
+                $('.tool-item').removeClass('selected');
+                $('#' + type).addClass('selected');
+                activeType = type;
+            }
+
+        });
+
+        var image = new Image();
+        image.src = imageData;
+        //image.width = 500;
+        //image.height = 380;
+        image.onload = function () {
+            canvasElement.width = image.width;
+            canvasElement.height = image.height;
+            $(canvasElement).css({ width: image.width, height: image.height });
+            ctx.drawImage(image, 0, 0);
+        }
+
+    });
+})();
---
+++ Archive/demos/GoogleMapCapture/scripts/main.js
@@ -0,0 +1,73 @@
+﻿(function () {
+
+    $(document).ready(function () {
+
+        function onCapture() {
+            //get transform value
+            var transform = $(".gm-style>div:first>div").css("transform");
+            var comp = transform.split(","); //split up the transform matrix
+            var mapleft = parseFloat(comp[4]); //get left value
+            var maptop = parseFloat(comp[5]);  //get top value
+            $(".gm-style>div:first>div").css({ //get the map container. not sure if stable
+                "transform": "none",
+                "left": mapleft,
+                "top": maptop,
+            });
+
+            $('.gmnoprint').each(function (node) {
+                this.attributes['skipelement'] = true;
+            });
+
+            html2canvas($('#googleMap')[0], {
+                logging: false,
+                useCORS: true,
+                sourceElement: $('#googleMap')[0],
+                onrendered: function (canvas) {
+                    console.log(canvas.toDataURL());
+                    openWindowWithPostRequest(canvas.toDataURL());
+                    $(".gm-style>div:first>div").css({ left: '', top: '', transform: transform });
+                    $('.gmnoprint').each(function (node) {
+                        this.attributes['skipelement'] = false;
+                    });
+                    //document.body.appendChild(canvas);
+                }
+            });
+        }
+
+        function initialize() {
+            var mapProp = {
+                center: new google.maps.LatLng(51.508742, -0.120850),
+                zoom: 5,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+            $('#button-capture').on("click", onCapture);
+        }
+        initialize();
+
+        function openWindowWithPostRequest(imageData) {
+            var winName = 'Mywindow';
+            var winURL = 'http://localhost:8003/demos/GoogleMapCapture/draw.debug.php';
+            var params = { 'imageData': imageData };
+            var form = document.createElement("form");
+            form.setAttribute("method", "post");
+            form.setAttribute("action", winURL);
+            form.setAttribute("target", winName);
+            for (var i in params) {
+                if (params.hasOwnProperty(i)) {
+                    var input = document.createElement('input');
+                    input.type = 'hidden';
+                    input.name = i;
+                    input.value = params[i];
+                    form.appendChild(input);
+                }
+            }
+            document.body.appendChild(form);
+            window.open('', winName);
+            form.target = winName;
+            form.submit();
+            document.body.removeChild(form);
+        }
+    });
+
+})();
